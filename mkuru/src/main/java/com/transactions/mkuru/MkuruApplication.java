21f79e0ae6677706f25ad42cd5b0693cf1183e98
Justice Macheyo <justice.macheyo@fin.africa>
2023-02-28 11:44:47 +0200

    Initial commit

---
+++ mkuru/src/main/java/com/transactions/mkuru/MkuruApplication.java
@@ -0,0 +1,14 @@
+package com.transactions.mkuru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkuruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MkuruApplication.class, args);
+	}
+
+}
+
---
+++ mkuru/src/test/java/com/transactions/mkuru/MkuruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transactions.mkuru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MkuruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
